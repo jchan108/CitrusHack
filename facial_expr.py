2b8352d880c7745657e8f444062807fbd8e31943
peraza97 <omarperaza19@gmail.com>
2018-04-21 16:54:53 -0700

    face

--- facial_expr.py
+++ facial_expr.py
@@ -8,7 +8,17 @@ def main():
     while True:
         _,frame = cap.read()
         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        cv2.imshow("frame",gray)
+
+        faces = face_model.detectMultiScale(gray, 1.3, 5)
+
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+        #show the frame
+        cv2.imshow("frame",frame)
+
+        #waitKey
         k = cv2.waitKey(1) & 0xFF;
         if k == 32:
             break
