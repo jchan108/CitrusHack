a805c20d1b3fccd7650fa40d8906bf7fac7858d3
peraza97 <omarperaza19@gmail.com>
2018-04-21 17:18:42 -0700

    smile

--- facial_expr.py
+++ facial_expr.py
@@ -3,6 +3,8 @@ import cv2
 
 def main():
     face_model = cv2.CascadeClassifier("cascades/face_cascade.xml")
+    smile_model = cv2.CascadeClassifier("cascades/smile_cascade.xml")
+    
     cap = cv2.VideoCapture(0)
 
     while True:
@@ -15,6 +17,15 @@ def main():
             cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
             roi_gray = gray[y:y+h, x:x+w]
             roi_color = frame[y:y+h, x:x+w]
+            smiles = smile_model.detectMultiScale(
+                roi_gray,
+                scaleFactor= 1.7,
+                minNeighbors= 22,
+                minSize = (25,25),
+                flags=cv2.CASCADE_SCALE_IMAGE)
+            for(sx,sy,sw,sh) in smiles:
+                cv2.rectangle(roi_color,(sx,sy),(sx+sw,sy+sh),(0,255,0),2)
+    
         #show the frame
         cv2.imshow("frame",frame)
 
