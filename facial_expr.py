e9235101d24159db60dd7eec1044db46b2487466
peraza97 <omarperaza19@gmail.com>
2018-04-21 18:02:08 -0700

    added threading

--- facial_expr.py
+++ facial_expr.py
@@ -1,37 +1,68 @@
 import numpy as np
 import cv2
 
+from threading import Thread
+import time
+
+
+#run video feed on a different thread
+class cameraFeed():
+    #init function
+    def __init__(self,source=0):
+        self.stream = cv2.VideoCapture(source)
+        _,self.frame = self.stream.read()
+        self.stopped = False
+    #start function
+    def start(self):
+        Thread(target=self.update, args=()).start()
+        return self
+    #update function
+    def update(self):
+        while True:
+            if self.stopped:
+                self.stream.release()
+                return
+            _,self.frame = self.stream.read()
+    #read function
+    def read(self):
+        return self.frame
+    #stop function
+    def stop(self):
+        self.stopped = True
+
+
 def main():
     face_model = cv2.CascadeClassifier("cascades/face_cascade.xml")
-    smile_model = cv2.CascadeClassifier("cascades/smile_cascade.xml")
-    
-    cap = cv2.VideoCapture(0)
-
+    mouth_model = cv2.CascadeClassifier("cascades/mouth_cascade.xml")
+    c = cameraFeed().start()
+    ref = c.read()
     while True:
-        _,frame = cap.read()
+        frame = c.read()
         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-
         faces = face_model.detectMultiScale(gray, 1.3, 5)
-
         for (x,y,w,h) in faces:
             cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
             roi_gray = gray[y:y+h, x:x+w]
             roi_color = frame[y:y+h, x:x+w]
-            smiles = smile_model.detectMultiScale(
+            mouth = mouth_model.detectMultiScale(
                 roi_gray,
                 scaleFactor= 1.7,
-                minNeighbors= 22,
+                minNeighbors= 25,
                 minSize = (25,25),
                 flags=cv2.CASCADE_SCALE_IMAGE)
-            for(sx,sy,sw,sh) in smiles:
+            #mouth classifier
+            for(sx,sy,sw,sh) in mouth:
+                if( sy > int(y + h*2/3)):
+                    continue
                 cv2.rectangle(roi_color,(sx,sy),(sx+sw,sy+sh),(0,255,0),2)
+    
     
         #show the frame
         cv2.imshow("frame",frame)
-
         #waitKey
         k = cv2.waitKey(1) & 0xFF;
         if k == 32:
+            c.stop()
             break
 
 if __name__ == '__main__':
